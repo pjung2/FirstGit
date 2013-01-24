151ee172f8dc48569c4fcc0fb7f8eed311df88a5
Chumples <Chumples@10.30.17.90>
2013-01-23 20:52:41 -0600

    Revert to last version

--- src/firstgit/FirstGit.java
+++ src/firstgit/FirstGit.java
@@ -10,7 +10,6 @@ public class FirstGit {
      * @param args the command line arguments
      */
     public static void main(String[] args) {
-        System.out.println("Hello Students");
-        System.out.println("Good bye");
+        System.out.println("Hello World");
     }
 }
