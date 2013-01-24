e744253bc90dc72837687db24898fbdd5680f731
pjung2@my.wctc.edu <>
2013-01-23 20:44:54 -0600

    First Commit

---
+++ src/firstgit/FirstGit.java
@@ -0,0 +1,15 @@
+package firstgit;
+
+/**
+ *
+ * @author Chumples
+ */
+public class FirstGit {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        System.out.println("Hello World");
+    }
+}
