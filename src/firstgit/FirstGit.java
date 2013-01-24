eaaf555e2f8a0f9afd4ebee6eb8d9c6fe98e7d7f
Chumples <Chumples@10.30.17.90>
2013-01-23 20:53:33 -0600

    Revert "Revert to last version"
    This reverts commit 151ee172f8dc48569c4fcc0fb7f8eed311df88a5.

--- src/firstgit/FirstGit.java
+++ src/firstgit/FirstGit.java
@@ -10,6 +10,7 @@ public class FirstGit {
      * @param args the command line arguments
      */
     public static void main(String[] args) {
-        System.out.println("Hello World");
+        System.out.println("Hello Students");
+        System.out.println("Good bye");
     }
 }
